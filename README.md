60bade2c00a44b4d93e9cb0c52e2259861628075
johnkirtley <kirtleyj16@gmail.com>
2020-08-14 13:37:54 -0700

    :fire: Custom React App Template Completed

---
+++ README.md
@@ -0,0 +1,1 @@
+# Custom React Template To Reduce Project Setup Time
---
+++ template/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className='App'>
+			<p>Let's Get It 🤟</p>
+		</div>
+	);
+};
+
+export default App;
